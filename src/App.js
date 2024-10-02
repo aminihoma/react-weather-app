34628ed8ecb1dd0ea83f5d1e772bc0a65b0f8bff
aminihoma <134835686+aminihoma@users.noreply.github.com>
2024-10-02 11:44:19 +0330

    add GitHub repository url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/homa-amini-ba327296"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Homa Amini
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/aminihoma/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }

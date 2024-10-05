cef46e09198ca23a9ec8c4a3007ab6a636e6213e
aminihoma <134835686+aminihoma@users.noreply.github.com>
2024-10-05 10:00:58 +0330

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,26 +3,32 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/homa-amini-ba327296"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Homa Amini
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/aminihoma/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/homa-amini-ba327296"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Homa Amini
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/aminihoma/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+          <br />
+          <button type="button" class="btn btn-success">
+            Success
+          </button>
+        </footer>
+      </div>
     </div>
   );
 }

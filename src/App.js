6f9494a3343916cdcb40f6427bb32f1224969fa1
aminihoma <134835686+aminihoma@users.noreply.github.com>
2024-10-14 12:54:59 +0330

    implement forecast for 5 days

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather city="tehran" />
+        <Weather city="london" />
 
         <footer>
           This project was coded by{" "}
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,61 +1,123 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 export default function WeatherForecast(props) {
+  let [ready, setReady] = useState(false);
+  let [forecast, setForecast] = useState(null);
+  function day(data) {
+    let day = data.getDay();
+    let week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return week[day];
+  }
   function handleresponse(response) {
     console.log(response.data);
+    setForecast(response.data.daily);
+    setReady(true);
   }
-  let apikey = "be81f193e065bf5feb2d944c7336968b";
-  let lon = props.data.coord.lon;
-  let lat = props.data.coord.lat;
-  let apiurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`;
-  axios.get(apiurl).then(handleresponse);
-  console.log(apiurl);
-  console.log(props.data.coord);
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatgerForecast-day">Thu</div>
-          <img src={props.data.icon} alt={props.data.description}></img>
-          <div className="WeatherForecast-temp">
-            <span className="WeatherForecast-temp-max">29°</span>
-            <span className="WeatherForecast-temp-min">17°</span>
+  console.log("forecast is...");
+  console.log(forecast);
+  if (ready) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <div className="WeatgerForecast-day">
+              {day(new Date(forecast[0].dt * 1000))}
+            </div>
+            <img
+              src={`https://openweathermap.org/img/wn/${forecast[0].weather[0].icon}@2x.png`}
+              alt={forecast[0].weather[0].description}
+            ></img>
+            <div className="WeatherForecast-temp">
+              <span className="WeatherForecast-temp-max">
+                {Math.round(forecast[0].temp.max)}°
+              </span>
+              <span className="WeatherForecast-temp-min">
+                {Math.round(forecast[0].temp.min)}°
+              </span>
+            </div>
           </div>
-        </div>
-        <div className="col">
-          <div className="WeatgerForecast-day">Thu</div>
-          <img src={props.data.icon} alt={props.data.description}></img>
-          <div className="WeatherForecast-temp">
-            <span className="WeatherForecast-temp-max">29°</span>
-            <span className="WeatherForecast-temp-min">17°</span>
+          <div className="col">
+            <div className="WeatgerForecast-day">
+              {day(new Date(forecast[1].dt * 1000))}
+            </div>
+            <img
+              src={`https://openweathermap.org/img/wn/${forecast[1].weather[0].icon}@2x.png`}
+              alt={forecast[1].weather[0].description}
+            ></img>
+            <div className="WeatherForecast-temp">
+              <span className="WeatherForecast-temp-max">
+                {Math.round(forecast[1].temp.max)}°
+              </span>
+              <span className="WeatherForecast-temp-min">
+                {Math.round(forecast[1].temp.min)}°
+              </span>
+            </div>
           </div>
-        </div>
-        <div className="col">
-          <div className="WeatgerForecast-day">Thu</div>
-          <img src={props.data.icon} alt={props.data.description}></img>
-          <div className="WeatherForecast-temp">
-            <span className="WeatherForecast-temp-max">29°</span>
-            <span className="WeatherForecast-temp-min">17°</span>
+          <div className="col">
+            <div className="WeatgerForecast-day">
+              {day(new Date(forecast[2].dt * 1000))}
+            </div>
+            <img
+              src={`https://openweathermap.org/img/wn/${forecast[2].weather[0].icon}@2x.png`}
+              alt={forecast[2].weather[0].description}
+            ></img>
+            <div className="WeatherForecast-temp">
+              <span className="WeatherForecast-temp-max">
+                {Math.round(forecast[2].temp.max)}°
+              </span>
+              <span className="WeatherForecast-temp-min">
+                {Math.round(forecast[2].temp.min)}°
+              </span>
+            </div>
           </div>
-        </div>
-        <div className="col">
-          <div className="WeatgerForecast-day">Thu</div>
-          <img src={props.data.icon} alt={props.data.description}></img>
-          <div className="WeatherForecast-temp">
-            <span className="WeatherForecast-temp-max">29°</span>
-            <span className="WeatherForecast-temp-min">17°</span>
+          <div className="col">
+            <div className="WeatgerForecast-day">
+              {day(new Date(forecast[3].dt * 1000))}
+            </div>
+            <img
+              src={`https://openweathermap.org/img/wn/${forecast[3].weather[0].icon}@2x.png`}
+              alt={forecast[3].weather[0].description}
+            ></img>
+            <div className="WeatherForecast-temp">
+              <span className="WeatherForecast-temp-max">
+                {Math.round(forecast[3].temp.max)}°
+              </span>
+              <span className="WeatherForecast-temp-min">
+                {Math.round(forecast[3].temp.min)}°
+              </span>
+            </div>
           </div>
-        </div>
-        <div className="col">
-          <div className="WeatgerForecast-day">Thu</div>
-          <img src={props.data.icon} alt={props.data.description}></img>
-          <div className="WeatherForecast-temp">
-            <span className="WeatherForecast-temp-max">29°</span>
-            <span className="WeatherForecast-temp-min">17°</span>
+          <div className="col">
+            <div className="WeatgerForecast-day">
+              {day(new Date(forecast[4].dt * 1000))}
+            </div>
+            <img
+              src={`https://openweathermap.org/img/wn/${forecast[4].weather[0].icon}@2x.png`}
+              alt={forecast[4].weather[0].description}
+            ></img>
+            <div className="WeatherForecast-temp">
+              <span className="WeatherForecast-temp-max">
+                {Math.round(forecast[4].temp.max)}°
+              </span>
+              <span className="WeatherForecast-temp-min">
+                {Math.round(forecast[4].temp.min)}°
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apikey = "5201594abea9f3e38b70e65b11a80c24";
+    let lon = props.data.coord.lon;
+    let lat = props.data.coord.lat;
+    let apiurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`;
+    axios.get(apiurl).then(handleresponse);
+    console.log("the forecast url is:....");
+    console.log(apiurl);
+    console.log(props.data.coord);
+
+    return null;
+  }
 }
